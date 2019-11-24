fa05244d2e16020db164d87226942ce158f34af7
osamah21-meet <57598420+osamah21-meet@users.noreply.github.com>
2019-11-24 19:42:37 +0200

    Add files via upload

--- facbook.py
+++ facbook.py
@@ -1,31 +1,36 @@
-class posts():
-	def __init__(self,user,content):
-		self.user=user
-		self.content=content
-
-class user():
-	def __init__(self,name,email,password):
-		self.name=name
-		self.email=email
-		self.password=password
-		self.list1=[]
-		self.list2=[]
-	def add_friend(self,email):
-		self.list1.append(email)
-
-		print(self.name+" has added " + email +" as a friend")
-	def remove_friend(self,email):
-		self.list1.remove(email)
-		print(self.name+" has removed "+email+" from friends")
-	def add_post(self,text):
-		self.list2.append(posts(self.name,text))
-		print(self.name+" has posted: "+text)
-	def get_userInfo(self):
-		print("name: "+self.name)
-		print("email: "+self.email)
-		print("password: "+self.password)
-		print("friends: "+str( self.list1))
-		print("post: "+ str(self.list2))
+#------------------------------------------------------------------------------------------------------#
+users=[]                                                                                               #
+posts=[]                                                                                               #
+class Post():                                                                                          #
+	def __init__(self,user,content):                                                                   #
+		self.user=user                                                                                 #
+		self.content=content                                                                           #
+class user():                                                                                          #
+	def __init__(self,name,email,password):                                                            #
+		users.append(self)                                                                             #
+		self.name=name                                                                                 #
+		self.email=email                                                                               #
+		self.password=password                                                                         #
+		self.list1=[]                                                                                  #
+	def add_friend(self,email):                                                                        #
+		self.list1.append(email)                                                                       #
+		print(self.name+" has added " + email +" as a friend")                                         #
+	def remove_friend(self,email):                                                                     #
+		self.list1.remove(email)                                                                       #
+		print(self.name+" has removed "+email+" from friends")                                         #
+	def add_post(self,text):                                                                           #
+		posts.append(Post(self.email,text))                                                            #
+		print(self.name+" has posted: "+text)                                                          #
+	def get_userInfo(self):                                                                            #
+		print("name: "+self.name)                                                                      #
+		print("email: "+self.email)                                                                    #
+		print("password: "+self.password)                                                              #
+		print("friends: "+str( self.list1))                                                            #
+		for x in posts:                                                                                #
+			if(self.email==x.user):                                                                    #
+				print("post: "+ str(x.content))                                                        #
+	def __repr__(self):                                                                                #
+		return self.name                                                                               #                                             
 #------------------------------------------------------------------------------------------------------#
 adam=user("adam","adam@gmail.com","adam123")                                                           #
 adam.add_friend("ahmad@gmail.com")                                                                     #
@@ -39,6 +44,15 @@ yazan=user("yazan","yazan@gmail.com","yazan123")                                
 yazan.add_friend("osama@gmail.com")                                                                    #
 yazan.add_friend("ahhmad@gmail.com")                                                                   #
 yazan.remove_friend("osama@gmail.com")                                                                 #
-yazan.add_post("mma for ever")                                                                         #
+yazan.add_post("x test x")                                                                             #
 yazan.get_userInfo()                                                                                   #
+#------------------------------------------------------------------------------------------------------#
+mo=user("yaza","yaza@gmail.com","yaza123")                                                             #
+mo.add_friend("ma@gmail.com")                                                                          #
+mo.add_friend("ad@gmail.com")                                                                          #
+mo.remove_friend("ma@gmail.com")                                                                       #
+mo.add_post("z test z")                                                                                #
+mo.get_userInfo()                                                                                      #
+#------------------------------------------------------------------------------------------------------#
+print(users)                                                                                           #
 #------------------------------------------------------------------------------------------------------#
